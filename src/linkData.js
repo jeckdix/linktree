e6642555b1ac2e297d46269aee684b263a5f6b55
Babajide Akinseye <bakinseye247@gmail.com>
2022-10-28 12:18:08 +0100

    Add link data

---
+++ src/linkData.js
@@ -0,0 +1,33 @@
+export const linkData = [
+  {
+    id: "twitter",
+    link: "https://twitter.com/bossjidex",
+    title: "Twitter Link",
+  },
+  {
+    id: "btn__zuri",
+    link: "https://training.zuri.team/",
+    title: "Zuri Team",
+  },
+  {
+    id: "books",
+    link: "http://books.zuri.team",
+    title: "Design and Coding books",
+    subtext: "this is where you find books about design and coding",
+  },
+  {
+    id: "book__python",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=<jeckdix>",
+    title: "Python Books",
+  },
+  {
+    id: "pitch",
+    link: "https://background.zuri.team",
+    title: "Background check for Coders",
+  },
+  {
+    id: "book__design",
+    link: "https://books.zuri.team/design-rules",
+    title: "Design Books",
+  },
+];
